5cf04b3e0d1c5519f525856be4c378bebf4c8a17
Kyle Nelson <kyle.st.aubyn@gmail.com>
2019-10-23 12:43:16 +0800

    updated css

--- src/App.js
+++ src/App.js
@@ -8,19 +8,24 @@ let post = [
     content: 'President Donald J. Trump in his unmatched wisdom withdraws forces from Syria so that he can pimp them out to Saudia Arabia',
     title: 'Armed Forces for Rent',
     author: 'By Christian Grewell', 
+    date: "Oct. 15, 2019",
   }, 
   {
-    content: 'After controversial comments from the Daryl Morey, General Manager of the Houston Rockets the Chinese government has made the decision to prohibit the streaming of games in the mainland',
+    content: 'After controversial comments from the Daryl Morey, General Manager of the Houston Rockets, the Chinese government has made the decision to prohibit the streaming of games in the mainland',
     title: 'NBA Games Banned In China',
-    author: 'By Oliver Steele', 
+    author: 'By Oliver Steele',
+    date: "Oct. 15, 2019", 
   },
   {
     content: 'I just needed a third story, nothing more, nothing less.',
     title: 'Much A Do About Nothing',
     author: 'By Every College Student Ever', 
+    date: "Oct. 15, 2019",
   }
 
 ];
+
+let buttons = ["World", "U.S.", "N.Y.", "Business", "Opinion", "Tech", "Science", "Health", "Sports", "Arts", "Books", "Style", "Food", "Travel", "Magazine", "T Magazine", "Real Estate", "Video"];
 
 function App() {
   return (
@@ -33,29 +38,32 @@ function App() {
             <div className="paper"><a href="">Today's Paper</a></div>
           </div>
           <div className="tabs">
-              <a href="">World</a>
-              <a href="">World</a>
-              <a href="">World</a>
-              <a href="">World</a>
+            {buttons.map(item => <a href="">{item}</a>)}
               
           </div>
         </div>
-        <div className="articleCard">
-          <div className="articleDate"><h3>Oct. 16, 2019</h3></div>
-          <div className="articleContent">
-            <div className="story">
-              <h2></h2>
-              <p></p>
-            </div>
-            <div className="author">
-              <h3> </h3>
-            </div>
-          </div>
-        </div>
+        {post.map(item => (<Article title = {item.title} content={item.content} author={item.author} date = {item.date}/>))}
 
       </div>
       
     </div>
+  );
+}
+
+function Article(props){
+  return(
+    <div className="articleCard">
+          <div className="articleDate"><h3>{props.date}</h3></div>
+          <div className="articleContent">
+            <div className="story">
+              <h2>{props.title}</h2>
+              <p>{props.content}</p>
+            </div>
+            <div className="author">
+              <h3>{props.author}</h3>
+            </div>
+          </div>
+        </div>
   );
 }
 
